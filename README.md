00a390eba18309b1ca008d38020ec976e3fac74c
Desuvader <desuvader@gmail.com>
2016-01-06 03:48:54 +0100

    Add new eexperiment and README

---
+++ README.md
@@ -0,0 +1,18 @@
+# eex - eexperiments
+
+> Collection of tiny *eexperiments* built with web technologies.
+
+Go ahead and visit the [project site](http://tofuness.github.io/eex/) if you haven't! I will try my best to regularly add new things to the repo
+and keep it interesting. Looking forward to learning lots of awesome things this year :goat:
+
+## Notes
+
+Ideally, all experiments should stay small and easily accessible. In other words, keep dependency bloat to a minimum. The goal is to inspire both *you* and me to build new *interesting* stuff for the web.
+
+## Install
+
+Clone the repo. Everything should run in the browser. Report any problems through the issue tracker.
+
+## License
+
+MIT © [Dennis Jin](https://github.com/tofuness/eex)
---
+++ fittext.js
@@ -0,0 +1,62 @@
+/*!
+* FitText.js 1.0 jQuery free version
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+* Modified by Slawomir Kolodziej http://slawekk.info
+*
+* Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
+*/
+(function(){
+  var css = function (el, prop) {
+    return window.getComputedStyle ? getComputedStyle(el).getPropertyValue(prop) : el.currentStyle[prop];
+  };
+
+  var addEvent = function (el, type, fn) {
+    if (el.addEventListener)
+      el.addEventListener(type, fn, false);
+		else
+			el.attachEvent('on'+type, fn);
+  };
+
+  var extend = function(obj,ext){
+    for(var key in ext)
+      if(ext.hasOwnProperty(key))
+        obj[key] = ext[key];
+    return obj;
+  };
+
+  window.fitText = function (el, kompressor, options) {
+
+    var settings = extend({
+      'minFontSize' : -1/0,
+      'maxFontSize' : 1/0
+    },options);
+
+    var fit = function (el) {
+      var compressor = kompressor || 1;
+
+      var resizer = function () {
+        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Bind events
+      // If you have any js library which support Events, replace this part
+      // and remove addEvent function (or use original jQuery version)
+      addEvent(window, 'resize', resizer);
+    };
+
+    if (el.length)
+      for(var i=0; i<el.length; i++)
+        fit(el[i]);
+    else
+      fit(el);
+
+    // return set of elements
+    return el;
+  };
+})();
