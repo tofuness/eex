3bf4666062f6720d0dfa3da50d5f509fdc7b8ea0
Desuvader <desuvader@gmail.com>
2016-06-22 03:17:29 +0200

    Almost finished with game of life experiment

---
+++ life/parse.js
@@ -0,0 +1,15 @@
+// Parses a 1.06 .lif file to stdout
+// Works on Node 6.10
+
+const fs = require('fs');
+const os = require('os');
+const util = require('util');
+
+const args = process.argv.splice(2);
+const input = fs.readFileSync(args[0], { encoding: 'utf-8' })
+              .split(os.EOL)
+              .filter((line) => line.indexOf('#') === -1)
+              .filter((line) => line !== '')
+              .map((line) => line.split(' ').map((offset) => parseInt(offset, 10)));
+
+console.log(JSON.stringify(input));
